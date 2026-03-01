bf3319b41117dd3e84bbd2df9034ffe6788450bd
Trent <trentunix@gmail.com>
2026-03-01 14:51:25 -0500

    LESSON1

---
+++ Lesson01/read-input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=/home/student/.local/bin:/home/student/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
+
+
+printf "%s\n" "Enter your name:"
+read NAME
+
+printf "%s\n" "Hello $NAME"
